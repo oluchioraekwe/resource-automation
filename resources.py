fdeee0f2cbd1256718a13c5068f565ba30593360
oluchioraekwe <70038042+oluchioraekwe@users.noreply.github.com>
2024-02-01 18:41:29 +0100

    edited python code

--- resources.py
+++ resources.py
@@ -36,7 +36,7 @@ list_rg = resource_client.resource_groups.list()
 #     f"Updated resource group {rg.name} in \
 #     the {rg.location} region with tags"
 # )
-print("List of resource groups")
+print("List of my resource groups")
 for rg in list_rg:
     print("Id: {}".format(rg.id))
     print("Name: {}".format(rg.name))
